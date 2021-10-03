a7c054ccd9662d7aa82c3d1239393f21c33be13a
OviRei <wasimchy100@gmail.com>
2021-10-03 19:18:25 +0100

    Added database

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.env" });
 
 const app = express();
 const port = 5000;
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+db.connect((error) => 
+{
+    if(error) console.log(error);
+    else console.log("MYSQL Connected...");
+});
 
 app.get("/", (req, res) => 
 {
