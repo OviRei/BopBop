4e39443148975409ce26062e9d2939c84b39a0da
OviRei <wasimchy100@gmail.com>
2021-11-06 23:50:42 +0000

    new error message design

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,33 @@
 /* eslint-disable indent */
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get("error");
+const btn = document.createElement("SPAN");
 
 switch(myParam)
 {
     case "Invaild_Credentials":
         document.getElementById("warningAlert").style.display = "block";
         document.getElementById("warningAlertText").innerHTML = "The credentials you have entered are invalid";
+        
+        btn.innerHTML = "&times;";
+        btn.classList.add("closebtn");
+        document.getElementById("warningAlertText").appendChild(btn);     
         break;
     default:
         document.getElementById("warningAlert").style.display = "none";
+}
+
+const close = document.getElementsByClassName("closebtn");
+let i;
+for(i = 0; i < close.length; i++) 
+{
+    close[i].onclick = function()
+    {
+        const div = this.parentElement.parentElement.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function() 
+        {
+            div.style.display = "none";
+        }, 600);
+    };
 }
--- public/js/register.js
+++ public/js/register.js
@@ -1,24 +1,52 @@
 /* eslint-disable indent */
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get("error");
+const btn = document.createElement("SPAN");
 
 switch(myParam)
 {
     case "Username_Inuse":
         document.getElementById("warningAlert").style.display = "block";
+        document.getElementById("warningAlert").style.marginLeft = "-165px";
         document.getElementById("warningAlertText").innerHTML = "That username is already in use";
-        document.getElementById("warningAlertText").scrollIntoView();
+
+        btn.innerHTML = "&times;";
+        btn.classList.add("closebtn");
+        document.getElementById("warningAlertText").appendChild(btn);   
         break;
     case "Email_Inuse":
         document.getElementById("warningAlert").style.display = "block";
+        document.getElementById("warningAlert").style.marginLeft = "-145px";
         document.getElementById("warningAlertText").innerHTML = "That email is already in use";
-        document.getElementById("warningAlertText").scrollIntoView();
+
+        btn.innerHTML = "&times;";
+        btn.classList.add("closebtn");
+        document.getElementById("warningAlertText").appendChild(btn);   
         break;
     case "Passwords_Dont_Match":
         document.getElementById("warningAlert").style.display = "block";
+        document.getElementById("warningAlert").style.marginLeft = "-153px";
         document.getElementById("warningAlertText").innerHTML = "Your passwords don't match";
-        document.getElementById("warningAlertText").scrollIntoView();
+
+        btn.innerHTML = "&times;";
+        btn.classList.add("closebtn");
+        document.getElementById("warningAlertText").appendChild(btn);   
         break;
     default:
         document.getElementById("warningAlert").style.display = "none";
+}
+
+const close = document.getElementsByClassName("closebtn");
+let i;
+for(i = 0; i < close.length; i++) 
+{
+    close[i].onclick = function()
+    {
+        const div = this.parentElement.parentElement.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function() 
+        {
+            div.style.display = "none";
+        }, 600);
+    };
 }
